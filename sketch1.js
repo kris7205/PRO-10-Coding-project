8478b7b58ac65a7cafabb8f8c878448f748acf16
kris7205 <111888789+kris7205@users.noreply.github.com>
2022-08-25 18:40:39 +0530

    Add files via upload

---
+++ sketch1.js
@@ -0,0 +1,7 @@
+console.log("hello I am telling you something about me")
+console.log("I am Krishna Singh i am fourtheen years old")
+console.log("I studing in class-8th,in section D")
+console.log("my school name is Wendy Scholl Junior Collage")
+console.log("My Hobi is descover new things and I like to do crafting")
+console.log("My father name is Yogesh Singh")
+console.log("My mother name is Sonali Singh")
